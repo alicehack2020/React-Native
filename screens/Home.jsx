d2b2b764b45ed4ad9e82fa93a1b07361cfa8ec87
Mangesh Pralhad Pandit <86043310+alicehack2020@users.noreply.github.com>
2023-06-09 09:31:27 +0530

    header in ract native

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,13 +1,44 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { defaultStyle } from '../styles/styles'
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+import { colors, defaultStyle } from "../styles/styles";
+import Header from "../components/Header";
+import { Avatar } from "react-native-paper";
 
 const Home = () => {
   return (
     <View style={defaultStyle}>
-      <Text>Vijay</Text>
-    </View>
-  )
-}
+      {/* header */}
+      <Header />
 
-export default Home
+      <View style={{
+        paddingTop: 100,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center"
+      }}>
+        
+        <View>
+          <Text style={{ fontSize: 20 }}>Our</Text>
+          <Text style={{ fontSize: 20, fontWeight: "900" }}>Products</Text>
+        </View>
+
+        <View>
+          <TouchableOpacity>
+            <Avatar.Icon
+              icon={"magnify"}
+              color="gray"
+              size={50}
+              style={{ backgroundColor: colors.color2, elevation: 12 }}
+            />
+          </TouchableOpacity>
+        </View>
+
+      </View>
+
+
+
+    </View>
+  );
+};
+
+export default Home;
